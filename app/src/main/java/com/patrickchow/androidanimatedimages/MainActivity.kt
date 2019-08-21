559936249d1d7d8f65b139c1371209040ed08c16
Patrick Chow <PatrickChow0803@gmail.com>
2019-08-21 16:43:39 -0400

    Added an animated Gif

--- app/src/main/java/com/patrickchow/androidanimatedimages/MainActivity.kt
+++ app/src/main/java/com/patrickchow/androidanimatedimages/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.patrickchow.androidanimatedimages
 
+import android.graphics.ImageDecoder
+import android.graphics.drawable.AnimatedImageDrawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val drawableIds = listOf(R.drawable.giphy)
+    var index = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        animateGif(R.drawable.giphy, animated_image)
     }
+
+    private fun animateGif(image: Int, view: ImageView) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val gifDrawable = ImageDecoder.decodeDrawable(ImageDecoder.createSource(resources, image))
+            view.setImageDrawable(gifDrawable)
+            (gifDrawable as AnimatedImageDrawable).start()
+        }
+    }
+
 }
