1e4891a36cb1c5896f37f8d3bb9ec86a1ad2405f
Patrick Chow <PatrickChow0803@gmail.com>
2019-08-21 21:03:48 -0400

    Added the animated vector and play/pause logic

--- app/src/main/java/com/patrickchow/androidanimatedimages/MainActivity.kt
+++ app/src/main/java/com/patrickchow/androidanimatedimages/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.patrickchow.androidanimatedimages
 
 import android.graphics.ImageDecoder
+import android.graphics.drawable.Animatable
 import android.graphics.drawable.AnimatedImageDrawable
 import android.graphics.drawable.AnimationDrawable
 import android.os.Build
@@ -14,14 +15,53 @@ class MainActivity : AppCompatActivity() {
 
     val drawableIds = listOf(R.drawable.giphy,
                              R.drawable.puppy_animated)
+
     var index = 0
+
+    var currentlyPlaying : Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       // animateGif(R.drawable.giphy, animated_image)
-        animateAnimationDrawable(R.drawable.puppy_animated, animated_image)
+        //Default play the hedgehog gif at start up
+        animateGif(drawableIds[index], animated_image)
+
+        iv_up.setOnClickListener{
+            increaseIndex()
+            animated_image.setImageDrawable(ContextCompat.getDrawable(this, drawableIds[index]))
+        }
+
+        iv_down.setOnClickListener {
+            decreaseIndex()
+            animated_image.setImageDrawable(ContextCompat.getDrawable(this, drawableIds[index]))
+
+        }
+
+        iv_change.setOnClickListener {
+
+            //If we're playing, then play the animation
+            if(currentlyPlaying) {
+                currentlyPlaying = false
+                //Change the image of the play button
+                animateVectorDrawable(R.drawable.avd_playtopause, it as ImageView)
+                //Depending on which index were're currently on, play that index from the drawableIds
+                when(index){
+                    0 -> animateGif(drawableIds[index], animated_image)
+                    1 -> animateAnimationDrawable(drawableIds[index], animated_image)
+                }
+            }
+            else{
+                //If we're not playing, then show the still image
+                currentlyPlaying = true
+                //Change the image of the play button
+                animateVectorDrawable(R.drawable.avd_pausetoplay, it as ImageView)
+                when(index){
+                    0 -> animated_image.setImageDrawable(ContextCompat.getDrawable(this, drawableIds[index]))
+                    1 -> animated_image.setImageDrawable(ContextCompat.getDrawable(this, drawableIds[index]))
+                }
+            }
+        }
     }
 
     //Animates the gif
@@ -38,6 +78,26 @@ class MainActivity : AppCompatActivity() {
         val frameDrawable = ContextCompat.getDrawable(this, id)
         view.setImageDrawable(frameDrawable)
         (frameDrawable as AnimationDrawable).start()
+    }
+
+    private fun animateVectorDrawable(id: Int, view: ImageView){
+        val animatedVectorDrawable = ContextCompat.getDrawable(this, id)
+        view.setImageDrawable(animatedVectorDrawable)
+        (animatedVectorDrawable as Animatable).start()
+    }
+
+    fun increaseIndex(){
+        index++
+        if(index >= drawableIds.size){
+            index = 0
+        }
+    }
+
+    fun decreaseIndex(){
+        index--
+        if(index < 0){
+            index = drawableIds.size-1
+        }
     }
 
 }
